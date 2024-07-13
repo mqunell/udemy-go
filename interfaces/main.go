10116ebcc793d6869b3c3b152d02c8f02c0b7aa4
Matt <mqunell@proton.me>
2024-07-13 13:24:40 -0700

    complete shapes assignment

--- interfaces/main.go
+++ interfaces/main.go
@@ -3,6 +3,7 @@ package main
 func main() {
 	interfaces_main()
 	http_main()
+	shapes_main()
 }
 
-// go run main.go interfaces.go http.go
+// go run main.go interfaces.go http.go shapes.go
---
+++ interfaces/shapes.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type shape interface {
+	getArea() float64
+}
+
+type triangle struct {
+	base   float64
+	height float64
+}
+
+type square struct {
+	sideLength float64
+}
+
+func shapes_main() {
+	t := triangle{base: 5.0, height: 4.0}
+	s := square{sideLength: 3.0}
+
+	printArea(t)
+	printArea(s)
+}
+
+func (t triangle) getArea() float64 {
+	return t.base * t.height * 0.5
+}
+
+func (s square) getArea() float64 {
+	return s.sideLength * s.sideLength
+}
+
+func printArea(s shape) {
+	fmt.Println(s.getArea())
+}
